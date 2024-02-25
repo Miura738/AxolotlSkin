a000ba78c72997cf2657a648215bd0df893b2758
AoMiura <miura738@1oli.cn>
2024-02-25 14:12:00 +0800

    测试下?

--- src/app/install/page.tsx
+++ src/app/install/page.tsx
@@ -1,11 +1,17 @@
 import Main from "@/app/install/main";
 import fs from "fs";
 import Custom404 from "@/app/not-found";
+import {Metadata} from "next";
+import getOption from "@/api/options";
+export const metadata: Metadata = {}
 
-export default function Home() {
+export default async function Home() {
 
 
   if (fs.existsSync(".env")) {
+    const title = await getOption("site_title") as string;
+    metadata.title = `404 NotFound > ${title}`;
+
     return <Custom404 />
   }
 
